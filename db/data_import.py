c478d06eca37944306498c8a5a1d51df02f70f8c
mbledkowski <pub@mble.dk>
2024-12-16 22:16:16 +0100

    Separate requirements for data_generation, and the rest of the project; add data_import script to db; update setup.sh

---
+++ db/data_import.py
@@ -0,0 +1,32 @@
+from neo4j import GraphDatabase
+
+URI = "bolt://localhost:7687"
+
+
+def main():
+    import_query()
+
+
+def import_query():
+    driver = GraphDatabase.driver(URI, auth=None)
+    query_accounts = """
+    LOAD CSV WITH HEADERS FROM 'file:///accounts.csv' AS row FIELDTERMINATOR ','
+    CREATE (acc: Account {id: row.id, name: row.name, email: row.email})-[r:LOGGED_ON]->(dev: Device {id: row.device_id})
+    """
+    query_transactions = """
+    LOAD CSV WITH HEADERS FROM 'file:///transactions.csv' AS row FIELDTERMINATOR ','
+    MATCH (source: Account {id: row.source_account_id}),(target: Account {id: row.target_account_id})
+    CREATE (source)-[r: SENT_TO {id: row.id, amount: toFloat(row.amount), timestamp: datetime(replace(row.timestamp, " ", "T"))}]->(target)
+    """
+    run_query(driver, query_accounts)
+    run_query(driver, query_transactions)
+
+
+def run_query(driver, query):
+    with driver.session() as session:
+        result = session.run(query)
+        return result.data()
+
+
+if __name__ == "__main__":
+    main()
--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 git lfs fetch --all
 git lfs pull
-# python3 -m venv .venv
-# source .venv/bin/activate
-# pip install -r requirements.txt
-(cd db && docker compose up -d)
+mkdir -p ./.neo4j/import
+cp ./data/*.csv ./.neo4j/import/
+docker compose -f ./db/docker-compose.yml up -d
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+python3 db/data_import.py
