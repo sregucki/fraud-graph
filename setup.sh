605dbc852414990b315241b0325389ac74986ae1
sregucki <sebastian.regucki@outlook.com>
2024-12-18 21:00:43 +0100

    Usuniecie zbędnego setup.sh i dodanie checkoutu bibliotek dla skryptów z /data-_generation

--- setup.sh
+++ setup.sh
@@ -5,6 +5,6 @@ cp ./data/*.csv ./.neo4j/import/
 docker compose -f ./db/docker-compose.yml up -d
 python3 -m venv .venv
 source .venv/bin/activate
-pip install -r requirements.txt
+pip install -r requirements.txt -r data_generation/requirements.txt
 while ! curl localhost:7687; do sleep 5;done
 python3 db/data_import.py
